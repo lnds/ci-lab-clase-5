84ee069bc52ce8b57a2fac291fbe028d71efa25b
Eduardo Díaz <ediaz@dcc8090.cl>
2022-10-19 11:07:35 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,14 @@ public class App {
     if (!(a + b > c && a + c > b && b + c > a)) {
       return "error no es un triangulo";
     }
+    if (a > b && a > c && esPitagorico(b, c, a)) {
+      return "es un triangulo pitagorico";
+    } else if (b > a && b > c && esPitagorico(a,c, b)) {
+      return "es un triangulo pitagorico";
+    } else if (c > a && c > b && esPitagorico(a,b,c)) {
+      return "es un triangulo pitagorico";
+    }
+        
     if (a == b && a == c && b == c) {
       return "es un triangulo equilatero";
     }
@@ -18,7 +26,11 @@ public class App {
     }
     return "es un triangulo escaleno";
   }
-
+  
+  bool esPitagorico(int a, int b, int c) {
+    return c*c == a*a + b*b;
+  }
+  
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
